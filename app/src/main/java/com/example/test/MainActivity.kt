f2aead19808781db18cd6698ed79dd651fc4a043
sojeong <sang-heon.kim@kt.com>
2021-12-20 17:04:52 +0900

    inflater

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
+    var container: FrameLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        container = findViewById<View>(R.id.container) as FrameLayout
+        val button = findViewById<View>(R.id.button) as Button
+        button.setOnClickListener {
+            val inflater =
+                getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            inflater.inflate(R.layout.sub1, container, true)
+        }
     }
 }
