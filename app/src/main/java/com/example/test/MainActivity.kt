34b8807f0914b47b8598ac9ac07cad866acd103c
sojeong <sang-heon.kim@kt.com>
2021-12-21 00:37:05 +0900

    List copy

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -4,13 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
+import android.widget.*
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gridView = findViewById<View>(R.id.gridView) as GridView
+
+        adapter = SingerAdapter()
+        adapter!!.addItem(SingerItem("소녀시대", "010-1000-1000", R.drawable.images))
+        adapter!!.addItem(SingerItem("걸스데이", "010-2000-2000", R.drawable.images))
+        adapter!!.addItem(SingerItem("여자친구", "010-3000-3000", R.drawable.images))
+        adapter!!.addItem(SingerItem("티아라", "010-4000-4000", R.drawable.images))
+        adapter!!.addItem(SingerItem("애프터스쿨", "010-5000-5000", R.drawable.images))
+
+        listView.adapter = adapter
+
+        listView.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+                val item = adapter!!.getItem(position) as SingerItem
+                Toast.makeText(applicationContext, "선택: " + item.name, Toast.LENGTH_LONG)
+                    .show()
+            }
     }
 
     class SingerAdapter : BaseAdapter() {
