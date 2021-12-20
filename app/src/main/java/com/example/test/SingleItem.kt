756cee45de2af3dff69d1ac86d2f06f6e8639dc7
sojeong <sang-heon.kim@kt.com>
2021-12-21 00:25:34 +0900

    initial commit

---
+++ app/src/main/java/com/example/test/SingleItem.kt
@@ -0,0 +1,11 @@
+package com.example.test
+
+class SingerItem(var name: String, var mobile: String) {
+
+    override fun toString(): String {
+        return "SingerItem{" +
+                "name='" + name + '\'' +
+                ", mobile='" + mobile + '\'' +
+                '}'
+    }
+}
