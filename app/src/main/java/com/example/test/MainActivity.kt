a8dce5d67a1f7ee6716b88282dd60573bd29b9ed
sojeong <sang-heon.kim@kt.com>
2021-12-22 17:53:23 +0900

    Intent를 이용하여 전화걸기

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.test
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    var editText: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editText = findViewById<View>(R.id.editText) as EditText
+
+        val button = findViewById<View>(R.id.button) as Button
+        button.setOnClickListener {
+            val receiver = editText!!.text.toString()
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$receiver"))
+            startActivity(intent)
+        }
     }
 }
