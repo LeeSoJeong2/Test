93882ed51f352ccb6a3cf3068742f5085dc93f1e
sojeong <sang-heon.kim@kt.com>
2022-01-07 15:54:25 +0900

    소켓 사용, 서버 & 클라이언트 통신

--- app/src/main/java/com/example/test/ChatService.kt
+++ app/src/main/java/com/example/test/ChatService.kt
@@ -1,32 +1,32 @@
 package com.example.test
 
+import android.app.Service
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.os.IBinder
 import android.util.Log
-import android.view.View
-import android.widget.Button
 import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
 import java.lang.Exception
 import java.net.ServerSocket
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class ChatService : Service() {
 
-        val button = findViewById<View>(R.id.button) as Button
-        button.setOnClickListener {
-//            val thread = MainActivity.ServerThread()
-//            thread.start()
-            val intent = Intent(applicationContext, ChatService::class.java)
-            startService(intent)
+    override fun onCreate() {
+        super.onCreate()
 
-        }
+        val thread = ServerThread()
+        thread.start()
     }
-    /*
-    internal class ServerThread : Thread() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    class ServerThread : Thread() {
         override fun run() {
             val port = 5001
             try {
@@ -47,5 +47,9 @@ class MainActivity : AppCompatActivity() {
                 e.printStackTrace()
             }
         }
-    } */
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
 }
